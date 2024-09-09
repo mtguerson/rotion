c3ef0524fe09b277793a16a5dd3d8a37ed83796c
mtguerson <mtguerson@gmail.com>
2024-09-09 19:52:16 -0400

    config: routing

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join, resolve } from 'node:path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { createFileRoute, createURLRoute } from 'electron-router-dom'
 import icon from '../../resources/icon.png'
 
 function createWindow(): void {
@@ -31,10 +32,20 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
+  const devServerURL = createURLRoute(
+    process.env.ELECTRON_RENDERER_URL!,
+    'main',
+  )
+
+  const fileRoute = createFileRoute(
+    join(__dirname, '../renderer/index.html'),
+    'main',
+  )
+
   if (is.dev && process.env.ELECTRON_RENDERER_URL) {
-    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
+    mainWindow.loadURL(devServerURL)
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(...fileRoute)
   }
 }
 
---
+++ src/renderer/src/Routes.tsx
@@ -0,0 +1,16 @@
+import { Router, Route } from 'electron-router-dom'
+import { Blank } from './pages/blank'
+import { Document } from './pages/document'
+
+export function Routes() {
+  return (
+    <Router
+      main={
+        <>
+          <Route path="/" element={<Blank />} />
+          <Route path="/document" element={<Document />} />
+        </>
+      }
+    />
+  )
+}

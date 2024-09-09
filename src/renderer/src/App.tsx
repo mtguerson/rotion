99e19d38721ccc9fcefda93682852cd206290723
mtguerson <mtguerson@gmail.com>
2024-09-09 19:56:04 -0400

    config: layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,16 +1,6 @@
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import { Routes } from './Routes'
 import './styles/global.css'
 
 export function App() {
-  return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
-      <Sidebar />
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }

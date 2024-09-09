a15fe18e9179362f77b111b11e314b8b50ed2b6f
mtguerson <mtguerson@gmail.com>
2024-09-09 17:50:03 -0400

    setup: tailwindcss setup

---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+

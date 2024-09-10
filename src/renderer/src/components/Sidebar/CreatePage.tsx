bb62d51feec171628c158839a0195c1060411894
mtguerson <mtguerson@gmail.com>
2024-09-10 19:46:13 -0400

    feat: create new document

--- src/renderer/src/components/Sidebar/CreatePage.tsx
+++ src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -1,8 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'phosphor-react'
+import type { Document } from '@shared/types/ipc'
 
 export function CreatePage() {
+  const queryClient = useQueryClient()
+
+  const { isPending: isCreatingNewDocument, mutateAsync: createDocument } =
+    useMutation({
+      mutationFn: async () => {
+        const response = await window.api.createDocument()
+
+        return response.data
+      },
+      onSuccess: (data) => {
+        queryClient.setQueryData(['documents'], (documents: Document[]) => {
+          return [...documents, data]
+        })
+      },
+    })
+
   return (
-    <button className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60">
+    <button
+      onClick={() => createDocument()}
+      disabled={isCreatingNewDocument}
+      className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60"
+    >
       <Plus className="h-4 w-4" />
       Novo documento
     </button>

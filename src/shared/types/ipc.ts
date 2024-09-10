b11837f7626a74a4351a7b3423c1a05ca58b33d4
mtguerson <mtguerson@gmail.com>
2024-09-10 18:02:36 -0400

    setup: crud structure by ipc

--- src/shared/types/ipc.ts
+++ src/shared/types/ipc.ts
@@ -1,9 +1,35 @@
 export interface Document {
   id: string
   title: string
-  content: string
+  content?: string
 }
+
+/**
+ * Request
+ */
+
+export interface SaveDocumentRequest extends Document {}
+
+export interface FetchDocumentRequest {
+  id: string
+}
+
+export interface DeleteDocumentRequest {
+  id: string
+}
+
+/**
+ * Response
+ */
 
 export interface FetchAllDocumentsResponse {
   data: Document[]
+}
+
+export interface FetchDocumentResponse {
+  data: Document
+}
+
+export interface CreateDocumentResponse {
+  data: Document
 }

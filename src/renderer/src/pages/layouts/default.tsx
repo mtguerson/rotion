bc84cde6076f55523091aedfca1337bc23839f53
mtguerson <mtguerson@gmail.com>
2024-09-09 22:29:04 -0400

    feat: add flex sidebar

--- src/renderer/src/pages/layouts/default.tsx
+++ src/renderer/src/pages/layouts/default.tsx
@@ -1,15 +1,23 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
+import { useState } from 'react'
 
 export function Default() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100 flex">
+    <Collapsible.Root
+      defaultOpen
+      onOpenChange={setIsSidebarOpen}
+      className="h-screen w-screen bg-rotion-900 text-rotion-100 flex"
+    >
       <Sidebar />
       <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
+        <Header isSidebarOpen={isSidebarOpen} />
         <Outlet />
       </div>
-    </div>
+    </Collapsible.Root>
   )
 }
